778e20f00ac61bb745155398ca07b44e7d53c22c
myrthew00 <willemsenmyrthe@gmail.com>
2022-05-20 10:26:59 +0200

    MSA, tree, start on visualization

---
+++ scripts/variability.py
@@ -0,0 +1,11 @@
+function 1:
+# input: MSA
+# output: entropy per position
+
+function 2:
+# input: entropy per position, window width l
+# output: averaged entropy over window length (e.g. kernalized)
+
+function 3:
+# input: averaged entropy over window length (e.g. kernalized)
+# output: visualization.
